42a78adcd885910b3cfc3e8c9d10983cad87d252
Redi <redicurrii@yahoo.com>
2024-09-11 19:20:10 +0200

    Added Cat Texts
    Got them from wikipedia

--- lib/tabs/strings.dart
+++ lib/tabs/strings.dart
@@ -1,6 +1,17 @@
 // lol im using this to store texts
 
 class Texts {
-  static const String loremipsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut sodales ligula, ac malesuada arcu. Vestibulum quis ligula quis risus rutrum finibus. Aenean sed dignissim mi. Ut egestas eget risus eget molestie. Fusce vitae lacus sed sapien dapibus volutpat ut et quam. Aliquam condimentum molestie pulvinar. Aenean ultrices eget libero sed ultricies. ";
+  static const String BritishShorthair='''The British Shorthair is the pedigreed version of the traditional British domestic cat, with a distinctively stocky body, thick coat, and broad face. The most familiar colour variant is the "British Blue", with a solid grey-blue coat, pineapple eyes, and a medium-sized tail. The breed has also been developed in a wide range of other colours and patterns, including tabby and colourpoint.''';
+  static const String PersianCat= '''The Persian cat, also known as the Persian Longhair, is a long-haired breed of cat characterised by a round face and short muzzle. The first documented ancestors of Persian cats might have been imported into Italy from Khorasan as early as around 1620, however, this has not been proven. Instead, there is stronger evidence for a longhaired cat breed being exported from Iran from the 19th century onwards. Persian cats have been widely recognised by the North-West European cat fancy since the 19th century, and after World War II by breeders from North America, Australia and New Zealand. Some cat fancier organisations' breed standards subsume the Himalayan and Exotic Shorthair as variants of this breed, while others generally treat them as separate breeds.''';
 static const String aboutme="Hi,\"mello\" is an app built so users can know different breeds of cats and take a quiz to find out which cat they are based on their personality. The app also provides links for animal protection services in the \"Support\" tab. Thank you, Redi.";
+static const String CairoCat='''
+The Cairo cat, often referred to as the "Cairo Street Cat," is a breed known for its adaptability and resilience. Originating from Egypt, these cats have a short, sleek coat and a muscular build. They are typically medium-sized with a well-proportioned body, large ears, and expressive eyes. Cairo cats are known for their friendly and sociable nature, making them great companions for families and individuals alike.
+  ''';
+  static const String SphynxCat='''
+The Sphynx cat is a distinctive breed known for its lack of fur and wrinkled skin. Originating from Canada, this breed has a unique appearance with a muscular, medium-sized body and large, bat-like ears. Despite their hairlessness, Sphynx cats have a warm, soft feel to their skin due to natural oils. They are known for their friendly, energetic, and playful demeanor, and they often seek human attention and companionship.
+  ''';
+  static const String SiameseCat = '''
+The Siamese cat is a breed renowned for its striking appearance and vocal personality. Originating from Thailand, Siamese cats have a sleek, slender body with a short coat and distinct color points on their ears, face, paws, and tail. They are known for their almond-shaped blue eyes and large, triangular ears. Siamese cats are highly social, intelligent, and vocal, often forming strong bonds with their owners and enjoying interactive play.
+  ''';
+
 }
